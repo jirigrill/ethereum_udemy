49a1ed46fca9844deb6191ce7b09962e3d9f07ba
Jiri Grill <jiri.grill@workday.com>
2021-07-09 10:36:16 +0200

    finished section 2

--- inbox/compile.js
+++ inbox/compile.js
@@ -6,5 +6,3 @@ const inboxpath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxpath, 'UTF-8');
 
 module.exports = solc.compile(source,1).contracts[':Inbox'];
-
-console.log('compiled')
